778e28afdc623508d58b08641197f67345e62c2f
Juan Carlos Fernández Pérez <jcfp0003@red.ujaen.es>
2024-07-01 17:47:34 +0200

    Display layout fix and menu animations

--- Scripts/GameControl.gd
+++ Scripts/GameControl.gd
@@ -36,7 +36,7 @@ func _play_button_press():
 
 # Game score button pressed, decrease count and relocate window randomly
 func _main_button_press():
-	game_score -= 1;
+	#game_score -= 1;
 	if game_score <= 0:
 		GAME_WINDOW.visible = false;
 		game_active = false;
@@ -65,10 +65,14 @@ func _move_win_to_rand():
 	var display_pos = DisplayServer.screen_get_position(target_display);
 	var display_siz = DisplayServer.screen_get_size(target_display);
 	# Pick random position in screen, limited by game window size (400), and add position offset (for multiscreen layouts)
-	gamewin_target_position.x = randi_range(display_pos.x, display_siz.x - 400) + display_pos.x;
-	gamewin_target_position.y = randi_range(display_pos.y, display_siz.y - 400) + display_pos.y;
+	gamewin_target_position.x = randi_range(0, display_siz.x - 400) + display_pos.x;
+	gamewin_target_position.y = randi_range(0, display_siz.y - 400) + display_pos.y;
 	
 	#print(target_display, gamewin_target_position);
 
 func _exit_pressed():
 	get_tree().quit();
+
+#func _ready():
+	#for i in DisplayServer.get_screen_count():
+		#print(DisplayServer.screen_get_position(i), DisplayServer.screen_get_size(i));
--- Scripts/TitleAnimation.gd
+++ Scripts/TitleAnimation.gd
@@ -1,15 +1,42 @@
-extends Label
+extends Control
+
+@onready var LABEL_TITLE : Label = $Title;
+@onready var ANIM_TREE_TITLE : AnimationTree = $TitleAnimTree;
 
 var shadow_hue = 0.0;
 var shadow_max_offset = 6.0;
 
-# Update shadow position randomly and color by hue each frame
+# Update title animations each frame
 func _process(delta):
-	self.label_settings.shadow_color = Color.from_hsv(shadow_hue, 1.0, 1.0, 1.0)
+	# Title shadow color by hue and shadow position randomly
+	LABEL_TITLE.label_settings.shadow_color = Color.from_hsv(shadow_hue, 1.0, 1.0, 1.0)
 	if shadow_hue < 1.0:
 		shadow_hue += 0.001
 	else:
 		shadow_hue = 0.0
-	
 	var added_offset = Vector2(randf_range(-shadow_max_offset, shadow_max_offset), randf_range(-shadow_max_offset, shadow_max_offset));
-	self.label_settings.shadow_offset = lerp(self.label_settings.shadow_offset, added_offset, 0.1);
+	LABEL_TITLE.label_settings.shadow_offset = lerp(LABEL_TITLE.label_settings.shadow_offset, added_offset, 0.1);
+
+func _play_btn_hovered():
+	ANIM_TREE_TITLE.set("parameters/conditions/playHovered", true);
+	ANIM_TREE_TITLE.set("parameters/conditions/playUnhovered", false);
+	ANIM_TREE_TITLE.set("parameters/conditions/exitHovered", false);
+	ANIM_TREE_TITLE.set("parameters/conditions/exitUnhovered", false);
+
+func _play_btn_unhovered():
+	ANIM_TREE_TITLE.set("parameters/conditions/playHovered", false);
+	ANIM_TREE_TITLE.set("parameters/conditions/playUnhovered", true);
+	ANIM_TREE_TITLE.set("parameters/conditions/exitHovered", false);
+	ANIM_TREE_TITLE.set("parameters/conditions/exitUnhovered", false);
+
+func _exit_btn_hovered():
+	ANIM_TREE_TITLE.set("parameters/conditions/playHovered", false);
+	ANIM_TREE_TITLE.set("parameters/conditions/playUnhovered", false);
+	ANIM_TREE_TITLE.set("parameters/conditions/exitHovered", true);
+	ANIM_TREE_TITLE.set("parameters/conditions/exitUnhovered", false);
+
+func _exit_btn_unhovered():
+	ANIM_TREE_TITLE.set("parameters/conditions/playHovered", false);
+	ANIM_TREE_TITLE.set("parameters/conditions/playUnhovered", false);
+	ANIM_TREE_TITLE.set("parameters/conditions/exitHovered", false);
+	ANIM_TREE_TITLE.set("parameters/conditions/exitUnhovered", true);
